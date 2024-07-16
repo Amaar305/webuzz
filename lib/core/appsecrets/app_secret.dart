f416f92ee216aaf7aa22f52181d73c2ca1672205
Amaar305 <uthmanameen2003@gmail.com>
2024-07-16 17:06:35 +0100

    initial

---
+++ lib/core/appsecrets/app_secret.dart
@@ -0,0 +1,10 @@
+class AppSecret {
+  
+  static const String appURL = '';
+  static const String appAnonKey = '';
+
+}
+
+// Platform  Firebase App Id
+// android   1:318852967476:android:5f7b8cf64c1ca0d357ee61
+// ios       1:318852967476:ios:21d3efdfe60df56f57ee61
